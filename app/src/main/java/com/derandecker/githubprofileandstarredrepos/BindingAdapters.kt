ea0345057c4743d9a1a5a2ef32b30f5b401037bf
Deran Decker <me@derandecker.com>
2022-01-19 20:57:54 -1000

    IndividualStarredRepo screen complete. Add BindingAdapters to convert Int to String then format.

--- app/src/main/java/com/derandecker/githubprofileandstarredrepos/BindingAdapters.kt
+++ app/src/main/java/com/derandecker/githubprofileandstarredrepos/BindingAdapters.kt
@@ -1,6 +1,7 @@
 package com.derandecker.githubprofileandstarredrepos
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.squareup.picasso.Picasso
 
@@ -9,4 +10,22 @@ fun bindProfileImage(imageView: ImageView, url: String?) {
         Picasso.get()
             .load(url)
             .into(imageView)
+}
+
+@BindingAdapter("watchersText")
+fun bindWatchers(textView: TextView, number: Int) {
+    val context = textView.context
+    textView.text = String.format("$number " + context.getString(R.string.watchers))
+}
+
+@BindingAdapter("forksText")
+fun bindForks(textView: TextView, number: Int) {
+    val context = textView.context
+    textView.text = String.format("$number " + context.getString(R.string.forks))
+}
+
+@BindingAdapter("starsText")
+fun bindStars(textView: TextView, number: Int) {
+    val context = textView.context
+    textView.text = String.format("$number " + context.getString(R.string.stars))
 }
